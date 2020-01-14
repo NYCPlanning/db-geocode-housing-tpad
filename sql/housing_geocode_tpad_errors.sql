7bed62498b0d421fd13fd4983724e31c2d9e9195
Ann Morris <annelizabethmorris@yahoo.com>
2020-01-14 15:28:05 -0500

    add house number, street name, borough to reports

--- sql/housing_geocode_tpad_errors.sql
+++ sql/housing_geocode_tpad_errors.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS housing_geocode_tpad_errors;
-SELECT geo_bbl, geo_bin, geo_tpad_new_bin, geo_tpad_new_bin_status,
+SELECT geo_hnum, geo_sname, geo_borough, geo_bbl, geo_bin,
+geo_tpad_new_bin, geo_tpad_new_bin_status,
 geo_tpad_dm_bin_status, geo_tpad_conflict_flag, geo_tpad_bin_status,
 geo_return_code, geo_return_code_2, geo_message, geo_message_2, geo_reason_code
 INTO housing_geocode_tpad_errors
