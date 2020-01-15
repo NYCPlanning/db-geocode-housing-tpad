f7d114b7c3e21ff1df48ca0a88ff6c23d5a9d713
Ann Morris <annelizabethmorris@yahoo.com>
2020-01-15 11:39:26 -0500

    use messages to detect records in tpad

--- sql/developments_coeff_geocoded_in_tpad.sql
+++ sql/developments_coeff_geocoded_in_tpad.sql
@@ -1,4 +1,5 @@
 CREATE TABLE developments_coeff_geocoded_in_tpad AS
 SELECT *
 FROM developments_coeff_geocoded
-WHERE geo_return_code IN ('22', '23');
+WHERE geo_message LIKE '%TPAD%'
+OR geo_message_2 LIKE '%TPAD%';
